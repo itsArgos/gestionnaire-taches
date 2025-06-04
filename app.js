50f485537482607b84a32f25ccfa310f153ca573
Adrien Martinaud <drop_m37@hotmail.com>
2025-06-04 19:27:02 +0200

    Add/Modified: - JS: new icon to see what priority is the task ( hight, medium or low ) - html/css: modification of the class name + new style for "priority"

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const form = document.getElementById("form");
 const submitForm = document.getElementById("submitForm");
 const closeForm = document.getElementById("closeForm");
 
-// Au clique sur le bouton "Ajouter une tâche", on affiche le formulaire
+// Affiche le formulaire
 onClickOpenForm.addEventListener("click", () => {
   form.style.display = "grid";
 });
 
-// Au clique sur le bouton "Annuler", on cache le formulaire
+// Cache et réinitialise le formulaire
 closeForm.addEventListener("click", () => {
   form.style.display = "none";
+  form.reset();
 });
 
 // Au clique sur le bouton "Ajouter la tâche" empêche la page de recharger
@@ -18,29 +19,38 @@ submitForm.addEventListener("click", (event) => {
   event.preventDefault();
 
   // Récuperation de la valeur de chaque champs du formulaire
-  const title = document.getElementById("title").value;
-  const description = document.getElementById("description").value;
-  const date = document.getElementById("deadline").value;
-  const priority = document.getElementById("priority").value;
+  const titleForm = document.getElementById("title-form").value;
+  const descriptionForm = document.getElementById("description-form").value;
+  const dateForm = document.getElementById("deadline-form").value;
+  const priorityForm = document.getElementById("priority-form").value;
 
   // Création d'un nouvel élément <li>
   const li = document.createElement("li");
   const taskList = document.getElementById("taskList");
 
-  // Création d'une icône de poubelle pour supprimer une tâche
+  // Création d'un "span" pour ajouter un icone de priorité à la suite
+  const taskContent = document.createElement("span");
+  taskContent.innerHTML = `<strong>${titleForm}</strong> - ${descriptionForm} - ${dateForm}`;
+
+  const priorityIcon = document.createElement("ion-icon");
+  priorityIcon.setAttribute("name", "ellipse");
+  priorityIcon.classList.add("priority-icon", `priority-${priorityForm}`);
+
   const icon = document.createElement("ion-icon");
   icon.setAttribute("name", "trash-outline");
   icon.style.cursor = "pointer";
 
-  // Ajoute le texte dans le "li" ( titre, déscription, date et priorité )
-  li.innerText = title + " - " + description + " - " + date + " - " + priority;
-
-  // Ajoute le li au parent ( taskList = ul )
-  taskList.appendChild(li);
-
-  // ajoute l'icone de poubelle à droite du texte pour supprimer la tâche (remove)
-  li.appendChild(icon);
+  // Suppression de la tâche (li)
   icon.addEventListener("click", () => {
     li.remove();
   });
+
+  // Ajout des éléments dans le <li> puis dans la liste
+  li.appendChild(taskContent);
+  li.appendChild(priorityIcon);
+  li.appendChild(icon);
+  taskList.appendChild(li);
+
+  // Réinitialisation du formulaire àprès ajout de la tâche
+  form.reset();
 });
